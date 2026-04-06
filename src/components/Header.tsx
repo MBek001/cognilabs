8407a9ceb943b17f0fe950522056a6eb4c929225
Abdurahmon <abdurahmanrahmanbekov@gmail.com>
2026-04-06 16:14:53 +0500

    feat: add responsive Header component with video modal and animated statistics

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -52,7 +52,7 @@ export default function Header() {
 							</button>
 							<iframe
 								className='w-full h-full'
-								src='https://youtu.be/sEjhXMkeDEA?si=QKcg2xabiRBy3Jlb'
+								src='https://www.youtube.com/embed/sEjhXMkeDEA'
 								title='YouTube video player'
 								allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture'
 								allowFullScreen
